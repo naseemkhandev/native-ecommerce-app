e010e441eef47744f46413a9732a732418cbba46
Naseem Khan <naseemsaffy@gmail.com>
2024-05-27 19:00:13 +0500

    feat: Add tailwind css setup

---
+++ client/App.jsx
@@ -0,0 +1,10 @@
+import {View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text className="text-3xl text-red-500">App</Text>
+    </View>
+  );
+};
+export default App;
