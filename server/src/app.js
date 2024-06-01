43d3986eca80d7604f0678bddeeeb289921e20b4
Naseem Khan <naseemsaffy@gmail.com>
2024-06-01 13:20:44 +0500

    chore: Add middleware for logging, JSON parsing, cookie parsing, and CORS

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,15 @@
 import express from "express";
 import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
+
+// Middlewares
 app.use(morgan("dev"));
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("<h1>Server is Running...</h1>");
