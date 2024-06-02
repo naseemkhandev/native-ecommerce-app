82f4dbc18834fca1e161992b679595a9707c77e1
Naseem Khan <naseemsaffy@gmail.com>
2024-06-02 21:24:12 +0500

    feat: Add new product functionality

--- server/src/utils/getDataUri.js
+++ server/src/utils/getDataUri.js
@@ -2,9 +2,12 @@ import path from "path";
 import dataURIParser from "datauri/parser.js";
 
 const getDataUri = (file) => {
+  if (!file) {
+    throw new Error("No file provided");
+  }
+
   const parser = new dataURIParser();
   const ext = path.extname(file.originalname).toString();
   return parser.format(ext, file.buffer);
 };
-
 export default getDataUri;
