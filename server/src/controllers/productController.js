93fff3009efe0ce5c199d98f48e0e3ce4bd2892b
Naseem Khan <naseemsaffy@gmail.com>
2024-06-02 21:24:12 +0500

    feat: Add new product functionality

--- server/src/controllers/productController.js
+++ server/src/controllers/productController.js
@@ -1,5 +1,9 @@
+import mongoose from "mongoose";
+import { v2 as cloudinary } from "cloudinary";
+
 import Product from "../models/ProductModel.js";
 import createError from "../utils/createError.js";
+import getDataUri from "../utils/getDataUri.js";
 
 export const getAllProducts = async (req, res, next) => {
   try {
@@ -18,12 +22,36 @@ export const getAllProducts = async (req, res, next) => {
 
 export const getSingleProduct = async (req, res, next) => {
   try {
-    const product = await Product.findById(req.params.id);
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id))
+      return next(createError(400, "Invalid Product ID"));
+
+    const product = await Product.findById(id);
 
     if (!product) return next(createError(404, "Product Not Found"));
 
     res.status(200).json({
       message: "Product Fetched Successfully",
+      product,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const addProduct = async (req, res, next) => {
+  try {
+    const file = getDataUri(req.file);
+    const uploadedImage = await cloudinary.uploader.upload(file.content);
+    const image = {
+      public_id: uploadedImage.public_id,
+      url: uploadedImage.secure_url,
+    };
+
+    const product = await Product.create({ ...req.body, images: [image] });
+
+    res.status(201).json({
+      message: "Product Added Successfully",
       product,
     });
   } catch (error) {
