277b993bbdf33c7a9823f771abf13c108e7a61cc
Naseem Khan <naseemsaffy@gmail.com>
2024-06-02 21:24:12 +0500

    feat: Add new product functionality

--- server/src/routes/productRoute.js
+++ server/src/routes/productRoute.js
@@ -2,10 +2,13 @@ import express from "express";
 import {
   getAllProducts,
   getSingleProduct,
+  addProduct,
 } from "../controllers/productController.js";
+import verifyToken from "../middlewares/verifyToken.js";
 const router = express.Router();
 
 router.get("/", getAllProducts);
 router.get("/find/:id", getSingleProduct);
+router.post("/add", verifyToken, addProduct);
 
 export default router;
