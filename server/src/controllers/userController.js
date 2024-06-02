f9d0d4932bb3a0db3b63b1742aa889b619b72af2
Naseem Khan <naseemsaffy@gmail.com>
2024-06-02 11:31:28 +0500

    feat: Update userController to include updateUserProfile functionality

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,3 +1,5 @@
+import bcrypt from "bcryptjs";
+
 import User from "../models/UserModel.js";
 
 export const getUserProfile = async (req, res, next) => {
@@ -7,6 +9,32 @@ export const getUserProfile = async (req, res, next) => {
     res
       .status(200)
       .json({ message: "User profile fetched successfully!", user });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateUserProfile = async (req, res, next) => {
+  try {
+    let user = req.body;
+
+    if (user.password) {
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(user.password, salt);
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+      req.user.userId,
+      {
+        $set: user,
+      },
+      { new: true }
+    ).select("-password");
+
+    res.status(200).json({
+      message: "User profile updated successfully!",
+      user: updatedUser,
+    });
   } catch (error) {
     next(error);
   }
--- server/src/routes/userRoute.js
+++ server/src/routes/userRoute.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { getUserProfile } from "../controllers/userController.js";
+import {
+  getUserProfile,
+  updateUserProfile,
+} from "../controllers/userController.js";
 import verifyToken from "../middlewares/verifyToken.js";
 const router = express.Router();
 
 router.get("/profile", verifyToken, getUserProfile);
+router.put("/profile/update", verifyToken, updateUserProfile);
 
 export default router;
